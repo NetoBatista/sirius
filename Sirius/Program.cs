d29258b2132dc88d8f5992561dfe083766e28182
Neto Santos <netosantosbatista@outlook.com>
2024-02-25 20:22:43 -0300

    add feature UseUrls

--- Sirius/Program.cs
+++ Sirius/Program.cs
@@ -8,6 +8,13 @@ builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 builder.Services.InjectDependency(builder.Configuration);
 builder.Services.ExecuteMigrations();
 
+var port = Environment.GetEnvironmentVariable("ASPNETCORE_HTTP_PORTS");
+if (!string.IsNullOrEmpty(port))
+{
+    builder.WebHost.UseUrls($"http://*:{port}");
+}
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +24,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
 
 app.UseHttpsRedirection();
 
